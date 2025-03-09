db5737275b7ee61cf28b6b658bfe998a6bc535b6
Serhii Mishovych <serhiimishovych@gmail.com>
2025-03-09 11:04:18 +0100

    First part of exercise

--- pattern_factory.py
+++ pattern_factory.py
@@ -1,22 +1,58 @@
-class Car:
+from abc import ABC, abstractmethod 
+
+class Vehicle(ABC):
     def __init__(self, make, model):
         self.make = make
         self.model = model
+
+    @abstractmethod
+    def start_engine(self):
+            pass
+
+class Car(Vehicle):
+    def __init__(self, make, model):
+        super().__init__(make, model)
 
     def start_engine(self):
         print(f"{self.make} {self.model}: Двигун запущено")
 
-class Motorcycle:
+class Motorcycle(Vehicle):
     def __init__(self, make, model):
-        self.make = make
-        self.model = model
+        super().__init__(make, model)
 
     def start_engine(self):
         print(f"{self.make} {self.model}: Мотор заведено")
 
-# Використання
-vehicle1 = Car("Toyota", "Corolla")
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make, model):
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make, model):
+        pass
+
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make, model):
+        return Car(make, f"{model} (US Spec)")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, f"{model} (US Spec)")
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make, model):
+        return Car(make, f"{model} (US Spec)")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, f"{model} (US Spec)")
+
+us_factory = USVehicleFactory()
+eu_factory = EUVehicleFactory()
+
+vehicle1 = us_factory.create_car("Ford", "Fusion")
 vehicle1.start_engine()
 
-vehicle2 = Motorcycle("Harley-Davidson", "Sportster")
+vehicle2 = eu_factory.create_motorcycle("Yamaha", "YZF-R1")
 vehicle2.start_engine()
+
+
